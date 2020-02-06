fe645c628e6046e761f7ff489cba6e752b827771
Promise Fru <promisefru1@gmail.com>
2020-02-06 14:24:04 +0100

    some logic added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = {
+        count: 1
+    };
+
     render() { 
         return (
             <React.Fragment>
-                <h1>first component</h1>
+                <span>{this.formatCounter()}</span>
                 <button>Increment</button>
             </React.Fragment>
         );
     }
+
+    formatCounter(){
+        if(this.state.count == 0){
+            return 'zero';
+        }else{
+            return this.state.count;
+        }
+    };
 }
  
 export default Counter;
